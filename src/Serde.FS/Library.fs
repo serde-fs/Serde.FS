541ad928935b39a836aba69e2772f359aa655530
Jordan Marr <jordanmarr@users.noreply.github.com>
2026-02-19 00:05:30 -0500

    Core types

--- src/Serde.FS/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Serde.FS
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ src/Serde.FS/Serde.fs
@@ -0,0 +1,24 @@
+﻿namespace Serde.FS
+
+/// Marker interface for backend-specific options.
+type ISerdeOptions = interface end
+
+type ISerdeBackend =
+    abstract Serialize : 'T * ISerdeOptions option -> string
+    abstract Deserialize : string * ISerdeOptions option -> 'T
+
+type Serde =
+    static member val DefaultBackend : ISerdeBackend =
+        failwith "No backend registered. Install a Serde.FS backend package."
+
+    static member Serialize(value: 'T) =
+        Serde.DefaultBackend.Serialize(value, None)
+
+    static member Serialize(value: 'T, options: ISerdeOptions) =
+        Serde.DefaultBackend.Serialize(value, Some options)
+
+    static member Deserialize<'T>(json: string) =
+        Serde.DefaultBackend.Deserialize<'T>(json, None)
+
+    static member Deserialize<'T>(json: string, options: ISerdeOptions) =
+        Serde.DefaultBackend.Deserialize<'T>(json, Some options)
