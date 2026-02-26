28282b4aa0f36f55740ac924e72d3052d05716c0
Jordan Marr <jordanmarr@users.noreply.github.com>
2026-02-25 23:08:55 -0500

    Updated sample record

--- src/Serde.FS.STJ.SampleApp/Program.fs
+++ src/Serde.FS.STJ.SampleApp/Program.fs
@@ -4,11 +4,11 @@ open Serde.FS
 open Serde.FS.STJ
 
 [<Serde>]
-type Person = { Name: string; Age: int }
+type Person = { Name: string; Age: int; LuckyNumbers: int Set }
 
 let run argv =
     SerdeStj.useAsDefault()
-    let person = { Name = "John"; Age = 30 }
+    let person = { Name = "John"; Age = 30; LuckyNumbers = Set [ 1; 2; 3 ] }
     let json = Serde.Serialize person
     printfn "Serialized: %s" json
     let deserialized: Person = Serde.Deserialize json
