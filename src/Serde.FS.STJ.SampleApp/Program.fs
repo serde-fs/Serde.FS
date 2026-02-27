4dab7e34641089101dc8b66f71c0bfd35ff31581
Jordan Marr <jordanmarr@users.noreply.github.com>
2026-02-27 13:13:49 -0500

    Updated SampleApp

--- src/Serde.FS.STJ.SampleApp/Program.fs
+++ src/Serde.FS.STJ.SampleApp/Program.fs
@@ -7,11 +7,21 @@ open Serde.FS.STJ
 type Address = { Street: string; City: string }
 
 [<Serde>]
-type Person = { Name: string; Age: int; Address: Address; LuckyNumbers: Option<int Set> }
+type Person = { 
+    Name: string
+    Age: int
+    Address: Address option
+    LuckyNumbers: int Set 
+}
 
 let run argv =
     SerdeStj.useAsDefault()
-    let person = { Name = "John"; Age = 30; Address = { Street = "123 Main St"; City = "Springfield" }; LuckyNumbers = Some (Set [ 1; 2; 3 ]) }
+    let person = { 
+        Name = "John"
+        Age = 30
+        Address = Some { Street = "123 Main St"; City = "Springfield" }
+        LuckyNumbers = Set [ 1; 2; 3 ] 
+    }
     let json = Serde.Serialize person
     printfn "Serialized: %s" json
     let deserialized: Person = Serde.Deserialize json
